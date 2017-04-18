ad14058a9089c4b77a337a312a908848f8111b61
DaniiarR <danraiymkulov@mail.com>
2017-04-18 19:07:38 +0600

    Now it's correct

--- package/module1.py
+++ package/module1.py
@@ -1,15 +1,14 @@
-def first(x):
-	if x>=1 and x<=3:
-		s = input("Enter some string:")
-        n = int(input("Enter the number of iterations:"))
-        for i in range(1,n+1):
-            print(s)
-	elif x>=4 and x<=6:
-		power = int(input("Enter the power of your number:"))
-        print(x**power)
-	elif x in range(7,10):
-		for x in range(x,x+x,1):
-            print(x)
-            x += 1
-    else:
-    	print("Error!")
+def one_three(x):
+	s = input("Enter some string:")
+    n = int(input("Enter the number of iterations:"))
+    for i in range(1,n+1):
+        print(s)
+def four_six(x):
+	power = int(input("Enter the power of your number:"))
+    print(x**power)
+def seven_ten(x):
+	for x in range(x,x+x,1):
+        print(x)
+        x += 1
+
+    	
