68ac29fcabdf0e8845385bb8adbe346e80ddbfa1
DaniiarR <danraiymkulov@mail.com>
2017-04-20 18:43:52 +0600

    Added pathfinder

--- Pathfinder.py
+++ Pathfinder.py
@@ -1,13 +1,15 @@
-import os,time
-import os.path
+import os,datetime
 path = input("Enter the path of your folder:\n")
 obj = input("Enter the name of your file:\n")
-while os.path.exists(path):
+if os.path.exists(path):
     files = os.listdir(path)
     if obj in files:
     	print("File directory: ",path)
-    	print(os.path.getsize(obj))
-    	print("File created:", time.asctime(time.localtime))
+    	print('File size: ',os.stat(obj).st_size)
+    	time = os.stat(obj).st_ctime
+    	print('File created: ',datetime.datetime.fromtimestamp(time))
+    	modtime = os.stat(obj).st_mtime
+    	print('Last modified: ',datetime.datetime.fromtimestamp(time)) 
 else:
 	print("There is no such a directory!")
 
