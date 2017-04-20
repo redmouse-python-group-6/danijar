bba73f3c2f8defd614aee6d4181bc6173e5f4881
DaniiarR <danraiymkulov@mail.com>
2017-04-20 21:45:14 +0600

    Updated again

--- hw1_second.py
+++ hw1_second.py
@@ -1,14 +1,14 @@
 print("Общество в начале XXI века")
 age = int(input("Enter your age:"))
-if age>0 and age<8:
+if 0<age<=7:
 	print("Вам в детский сад")
-elif age>7 and age<19:
+elif 7<age<=18:
 	print("Вам в школу")
-elif age>18 and age<26:
+elif 18<age<=25:
 	print("Вам в профессиональное учебное заведение")
-elif age>25 and age<=60:
+elif 25<age<=60:
 	print('Вам на работу')
-elif age>=60 and age<=120:
+elif 60<age<=120:
 	print("Вам предоставляется выбор")
 elif age<0 or age>120:
 	print("Эта программа для людей!\n"*5)
