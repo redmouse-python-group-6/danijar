d384217d9ea3e5e7f2dc2dc9e4e144c7fe623b6f
DaniiarR <danraiymkulov@mail.com>
2017-04-24 22:49:13 +0600

    Deleted wrong scripts

--- hw1_updated.py
+++
@@ -1,9 +0,0 @@
-from package import module1
-x = int(input("Enter the number from 1 to 9:\n"))
-if x in range(1,4):
-	module1.one_three(x)
-elif x in range(4,7):
-	module1.four_six(x)
-elif x in range(7,10):
-	module1.seven_ten(x):
-	
--- word_len.py
+++
@@ -1,6 +0,0 @@
-x = input("Enter some string:\n")
-def word(x):
-	global x
-    for i in x.split( ) :
-        return(i,"- {}".format(len(i)) )
-print(word(x))
